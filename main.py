37a5cb4c5c18349818e4ef6641b2cdfe9e736796
Anastasiia Efremova <e-asja@yandex.ru>
2026-05-30 16:47:17 +0300

    Update main.py with latest changes

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 print('Hello from repository!')
 print('This is an update from my local machine!')
+import os
+from dotenv import load_dotenv
+
+# Загружаем переменные окружения из .env
+load_dotenv()
+
+def print_author():
+    # Читаем значение переменной AUTHOR из .env и присваиваем его переменной author
+    author = os.getenv('AUTHOR')
+    print(f"Автор проекта: {author}")
+
+# Проверяем работу функции
+print_author()
+
 
 
